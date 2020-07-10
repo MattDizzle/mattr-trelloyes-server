6843c369f8271305d23d5d9a4cd024212aa55b48
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-09 22:46:33 -0700

    added api token

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const winston = require('winston');
 
 const app = express();
 
@@ -14,6 +15,44 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+const cards = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+}];
+
+const lists = [{
+  id: 1,
+  header: 'List One',
+  cardIds: [1]
+}];
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
 
 app.get('/', (req, res) => {
   // eslint-disable-next-line semi
